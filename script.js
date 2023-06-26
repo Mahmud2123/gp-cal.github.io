7e7c2f0780d3b13e0f55e243e31eb0590863e4df
Mahmud2123 <92996758+Mahmud2123@users.noreply.github.com>
2023-06-26 21:34:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let grade = parseFloat(document.getElementById("grade").value);
       
 
 document.getElementById("btn-cal").addEventListener("click", ()=>{
-  var totalUnit = unit1 + unit2 + unit3 + unit4;
-      var totalGrade = (grade1 * credit1) + (grade2 * credit2) + (grade3 * credit3) + (grade4 * credit4);
+  var totalUnits = unit1 + unit2 + unit3 + unit4 + unit5 + unit6 + unit7 + unit8 + unit9+ unit10;
+      var totalGrades = (grade1 * unit1) + (grade2 * unit2) + (grade3 * unit3) + (grade4 * unit4) +  (grade5 * unit5) + (grade6 * unit6) + (grade7 * unit7) + (grade8 * unit8) + (grade9 * unit9) + (grade10 * unit10);
+var gpa = totalGrades / totalUnits;
+
+      var result = document.getElementById("result");
+      result.value = "GPA: " + gpa.toFixed(2); 
+
 });
