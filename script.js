48a36354b09b6261b765ebf3fd52073aaab1654a
Mahmud2123 <92996758+Mahmud2123@users.noreply.github.com>
2023-06-26 21:24:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let unit = parseFloat(document.getElementById("unit").value);
+let grade = parseFloat(document.getElementById("grade").value);
+      
+
 document.getElementById("btn-cal").addEventListener("click", ()=>{
-  alert("you are welcome to BASUG")
+  var totalUnit = unit1 + unit2 + unit3 + unit4;
+      var totalGrade = (grade1 * credit1) + (grade2 * credit2) + (grade3 * credit3) + (grade4 * credit4);
 });
