e3bab74cd434d34abeffc2537ee2525c79e7cd1b
Mahmud2123 <92996758+Mahmud2123@users.noreply.github.com>
2023-06-27 01:38:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-var selectUnit = (document.getElementById("unit1");
+document.getElementById("btn-cal").addEventListener("click", () => {
+      var selectUnit = document.querySelectorAll('select[id^="unit"]');
+      var sum = 0;
 
-var selectGrade4 = parseFloat(document.getElementById("grade").value);
-
-
-document.getElementById("btn-cal").addEventListener("click", ()=>{
-  var sum = 0;
-  for (var i = 0; i < selectUnit.length; i++) {
+      for (var i = 0; i < selectUnit.length; i++) {
         var value = parseFloat(selectUnit[i].value);
         if (!isNaN(value)) {
           sum += value;
-          var totalUnits = sum;
         }
       }
 
-  
-  var unitBox = document.getElementById("totalUnit");
-  unitBox.value = totalUnits
-      var totalGrades =  selectGrade1 + selectGrade2 + selectGrade3 + selectGrade4 + selectGrade5
-var gradeBox = document.getElementById("totalGrade");
-  gradeBox.value = totalGrades
-  var UnitGrade = totalUnits * totalGrades
-  var gpa = UnitGrade / totalUnits;
-
-      var result = document.getElementById("result");
-      result.value = gpa.toFixed(2); 
-
-});
+      var unitBox = document.getElementById("totalUnit");
+      unitBox.value = sum;
+    });
